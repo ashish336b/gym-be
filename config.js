769bc5196cf03780d40420dc4f67aac9dbeafbdc
ashish336b <ashish336b@gmail.com>
2020-11-15 13:44:34 +0545

    test

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI: "mongodb://localhost:27017/gym-be",
-  port: 5000,
-  baseUrl: `http://localhost:5000`,
+  port: 3000,
+  baseUrl: `http://localhost:3000`,
   isDev: true,
 };
