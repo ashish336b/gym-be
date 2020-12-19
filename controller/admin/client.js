59e06659bea9f90753a03318cae5ee764897c269
ashish336b <ashish336b@gmail.com>
2020-12-19 21:00:54 +0545

    list all payment made for trainer

--- controller/admin/client.js
+++ controller/admin/client.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const clientModel = require("../../models/clientModel");
 const paymentModel = require("../../models/paymentModel");
+const objectId = require("mongoose").Types.ObjectId;
 /**
  * method : GET
  * url : /admin/client
@@ -24,6 +25,28 @@ router.get("/paymentList", async (req, res, next) => {
     },
   });
   res.json({ error: null, data: paymentList });
+});
+/**
+ * method : GET
+ * url : /admin/client/paymentList/:trainerId
+ * Desc : list of all payment paid for specific trainer
+ */
+router.get("/paymentList/:trainerId", async (req, res, next) => {
+  let paymentList = await paymentModel
+    .find({
+      isDeleted: false,
+    })
+    .populate({
+      path: "request",
+      populate: {
+        path: "clientId trainerId",
+        select: { name: 1, role: 1 },
+      },
+    });
+  paymentList = paymentList.filter((el) => {
+    return el.request.trainerId._id.toString() === req.params.trainerId;
+  });
+  res.json(paymentList);
 });
 /**
  * method : GET
