b5316b3e179343c1441edbfa22c9da24acbf13d6
ashish336b <ashish336b@gmail.com>
2020-12-23 23:34:08 +0545

    email html rendered

--- helpers/Auth.js
+++ helpers/Auth.js
@@ -44,11 +44,31 @@ class Auth {
         expiresIn: "1h",
       });
       let url = `http://localhost:3000/admin/forgotPassword/${token}`;
+      let html = `<!doctype html>
+      <html>
+        <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        </head>
+        <body style="font-family: sans-serif;">
+          <div style="display: block; margin: auto; max-width: 600px;" class="main">
+            <h1 style="font-size: 18px; font-weight: bold; margin-top: 20px">Shrijan Malakar Fitness House</h1>
+            <p>Your token is:</p>
+            
+            <p>${token}</p>
+            <h1 style="color : red">Warning! Remember Not to share this token</h1>
+          </div>
+          <!-- Example of invalid for email html/css, will be detected by Mailtrap: -->
+          <style>
+            .main { background-color: white; }
+            a:hover { border-left-width: 1em; min-height: 2em; }
+          </style>
+        </body>
+      </html>`;
       new Mailer().createConnection().sendMail({
         from: "shrijan@gmail.com",
         to: params.email,
         subject: "Password Reset Link",
-        html: url,
+        html: html,
       });
       return {
         error: null,
